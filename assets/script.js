9d55cfd6d14aede84b6b506c73fffe7452b18b40
Asika Habib <asikahabib@gmail.com>
2022-04-06 23:10:58 -0700

    added hashtags to the load functionality

--- assets/script.js
+++ assets/script.js
@@ -50,15 +50,16 @@ function hoursTime(){
 }
 
 //need to load each saved item per hour through jquery
-$("9 .text").val(localStorage.getItem("9"));
-$("10 .text").val(localStorage.getItem("10"));
-$("11 .text").val(localStorage.getItem("11"));
-$("12 .text").val(localStorage.getItem("12"));
-$("13 .text").val(localStorage.getItem("13"));
-$("14 .text").val(localStorage.getItem("14"));
-$("15 .text").val(localStorage.getItem("15"));
-$("16 .text").val(localStorage.getItem("16"));
-$("17 .text").val(localStorage.getItem("17"));
+//had to add the # to specify the ids
+$("#9 .text").val(localStorage.getItem("9"));
+$("#10 .text").val(localStorage.getItem("10"));
+$("#11 .text").val(localStorage.getItem("11"));
+$("#12 .text").val(localStorage.getItem("12"));
+$("#13 .text").val(localStorage.getItem("13"));
+$("#14 .text").val(localStorage.getItem("14"));
+$("#15 .text").val(localStorage.getItem("15"));
+$("#16 .text").val(localStorage.getItem("16"));
+$("#17 .text").val(localStorage.getItem("17"));
 hoursTime();
 
 })
